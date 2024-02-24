217d2861182a704afb4aabbdaa4eb981828f2de7
regis258 <reginaldopalafoz@gmail.com>
2024-02-23 21:41:00 -0300

    atualização

---
+++ index.js
@@ -0,0 +1,26 @@
+function carregaImagem1(){
+    document.getElementById("logo1").src = "1.png";
+}
+function carregaImagem2(){
+    document.getElementById("logo1").src = "2.png";
+}
+function carregaImagem3(){
+    document.getElementById("logo1").src = "3.png";
+}
+function carregaImagem4(){
+    document.getElementById("logo1").src = "4.png";
+}
+function carregaImagem5(){
+    document.getElementById("logo1").src = "5.png";
+}
+function carregaImagem6(){
+    document.getElementById("logo1").src = "6.png";
+}
+function carregaImagem7(){
+    document.getElementById("logo1").src = "imgs/Colorful Modern Creative Logo (1).png";
+}
+
+    /*document.querySelector("glow-on-hover").src = imagem2;
+    document.querySelector("glow-on-hover").src = imagem3;
+    document.querySelector("glow-on-hover").src = imagem4;
+    document.querySelector("glow-on-hover").src = imagem5;*/
